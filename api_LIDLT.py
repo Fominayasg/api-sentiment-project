3bdc8aeb27acf82eac8c4d76e810e9ebf9f49177
Sonia Fominaya <fominayasg@gmail.com>
2021-02-23 02:04:39 +0100

    a

---
+++ api_LIDLT.py
@@ -0,0 +1,58 @@
+from flask import Flask,request
+
+# Import from the mongoConnection file my generic functions
+from mongoConnection import read_data
+from mongoConnection import insert_data
+
+from bson import json_util, ObjectId
+
+
+app = Flask("LIDLT")
+
+@app.route("/inicio")
+def inicio():
+    return "Si no conoces el reality La Isla de las tentaciones visita:  https://www.telecinco.es/la-isla-de-las-tentaciones/"
+
+# "Get" endpoints
+
+@app.route("/mensajes")
+def mensajes(): 
+    data = read_data({},"Frases", project = {"_id":0,"Mensaje":1})
+    return json_util.dumps(data)
+
+@app.route("/participantes")
+def participantes(): 
+    data = read_data({},"Participantes", project = {"_id":0,"Nombre":1})
+    return json_util.dumps(data)
+
+@app.route("/participantes/details/<participante_id>")
+def participantes_details(participante_id):
+    try: #hacemos un try/exept para prevenir fallos si se introduce un "participante_id" que no sea un ObjectId
+        id_ = ObjectId(participante_id) 
+    except: 
+        return {"Error": "participante_id no válido"},400
+       
+    q = {"_id": id_}
+    data = read_data (q,"Participantes")
+    if len(data) == 0:
+        return{"error":"No hay ningun participante con ese id"}
+    return json_util.dumps(data)
+
+# "Post"  endpoints
+
+@app.route("/participantes/new")
+def participantes_new():
+    args = dict(request.args)
+    if "Nombre" not in args.keys(): 
+        return {"Error": "Nombre es un parámetro obligatorio"}
+                
+        # Para asegurarnos de no duplicar participantes hacemos lo siguiente:
+    q = {"Nombre":args["Nombre"]}
+    data =read_data(q,"Participantes")
+    if len(data)>1:
+        return{"Error": "El participante ya está registrado"}
+
+    res = insert_data(args,"Participantes")
+    return json_util.dumps({"_id":res})
+
+app.run(debug = True)
---
+++ mongoConnection.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+client = MongoClient()
+
+db = client.LIDLT
+'''db.create_collection(name = 'Frases')
+db.create_collection(name= 'Grupos')
+db.create_collection(name= 'Participantes')'''
+
+def insert_data (coll,data,database = db):
+    res = database[coll].insert_one (data)
+    return res.inserted_id
+
+def read_data (query,coll,database = db, project=None):
+    data= database[coll].find(query,project)
+    return list(data)
+
+def delete_data(coll,data,database = db):
+    deleted = database[coll].remove(data)
+    return f"You deleted {data} from {coll}"
